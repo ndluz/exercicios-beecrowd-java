82a5a25854504977628c376f8f801e090692d5c0
Andy <anderson54644@gmail.com>
2024-05-02 22:19:43 -0300

    wrong answer 75%

--- resolucoes/src/brind_face_2015/Program.java
+++ resolucoes/src/brind_face_2015/Program.java
@@ -8,47 +8,46 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int amount = sc.nextInt();
+		
+		if (amount <= 1) {
+			System.exit(1);
+		}
+		
 		int giftsCount = 0;
 		String studentLetters;
 		
 		boolean isEqual = false;
 		
 		List<String> letters = new ArrayList<String>();
-		letters.add("FACE");
+		letters.add("F A C E");
 		
-		for (int i = 0; i < amount; i++) {
-			studentLetters = sc.next();
-			letters.add(studentLetters);
-		}
-		
-		int i = 1;
-		while(letters.size() > 1) {
-			StringBuilder normal = new StringBuilder(letters.get(i - 1));
-			StringBuilder reversed = new StringBuilder(letters.get(i));
+		for (int i = 0; i <= amount; i++) {
 			
+			studentLetters = sc.nextLine();
+			letters.add(studentLetters);
+			//System.out.println(letters);
+			
+			StringBuilder normal = new StringBuilder(letters.get(letters.size() - 2));
+			StringBuilder reversed = new StringBuilder(letters.get(letters.size()- 1));
 			isEqual = 0 == normal.compareTo(reversed.reverse());
-			System.out.println(isEqual);
-			System.out.println(normal);
-			System.out.println(reversed);
+			
+			//System.out.println(isEqual);
+			//System.out.println(normal + " normal");
+			//System.out.println(reversed + " reversed");
 			
 			if (isEqual) {
 				giftsCount++;
-				try {
-					letters.remove(i);
-					letters.remove(i-1);
-					i = 1;
+				
+				letters.remove(letters.size()-1);
+				letters.remove(letters.size()-1);
+				
+				if (letters.size() == 0) {
+					letters.add("F A C E");
+					continue;
 					
-					
-				} catch (Exception e) {
-					letters.add("FACE");
-					i = 1;
 				}
 			}
-			
-			if (i == amount) {
-				break;
-			}
-			i++;
+			//System.out.println(letters);
 		}
 		
 		System.out.println(giftsCount);
