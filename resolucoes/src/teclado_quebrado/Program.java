6672de6377ca0db65f81a18df59e3912fafdcc24
Andy <anderson54644@gmail.com>
2024-05-04 21:58:56 -0300

    adicionando problema teclado quebrado

---
+++ resolucoes/src/teclado_quebrado/Program.java
@@ -0,0 +1,18 @@
+package teclado_quebrado;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String entry;
+		String[] res;
+		
+		
+		while(sc.hasNext()) {
+			entry = sc.nextLine();
+			
+		}
+	}
+}
