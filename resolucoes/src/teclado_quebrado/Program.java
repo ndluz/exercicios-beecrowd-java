9375370715908715c38a2676929b8496e622801a
Andy <anderson54644@gmail.com>
2024-05-05 22:40:21 -0300

    tentando usar linkedList

--- resolucoes/src/teclado_quebrado/Program.java
+++ resolucoes/src/teclado_quebrado/Program.java
@@ -1,5 +1,6 @@
 package teclado_quebrado;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Program {
@@ -7,12 +8,19 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String entry;
-		String[] res;
-		
+		String[] aux;
+		boolean isHome = false;
+		LinkedList<String> res =  new LinkedList<>();
 		
 		while(sc.hasNext()) {
 			entry = sc.nextLine();
+			aux = entry.split("");
+			for (int i = 0; i < aux.length; i++) {
+				
 			
+			
+			}
 		}
+		
 	}
 }
